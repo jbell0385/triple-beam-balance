2f126b3a6b39e8207288c88b16f5b0a10a034aab
Jeremy Bell <jbell0385@gmail.com>
2018-05-25 12:03:08 -0700

    commit to heroku

--- app.js
+++ app.js
@@ -1,46 +1,55 @@
+var canvas;
 
-var img, x1, y1, x2, y2, x3, y3, triangle01;
-var triangleArr = [];
+var beamBg;
+var rider01;
+var rider02;
+var rider03;
+var rider01X = 32;
+
+// var mx = mouseX;
+// var my = mouseY;
 
 function preload(){
-    img=loadImage('images/triple-beam-balance.png');
+    beamBg=loadImage('/images/beam-bg.png');
+    rider01 = loadImage('/images/rider01.png');
 }
 
 function setup(){
-    createCanvas(640,480);
-    image(img,0,0);
+    canvas = createCanvas(558,271);
 
-    triangle01 = {
-        x1 : 54,
-        y1 : 27,
-        x2 : 72,
-        y2 : 27,
-        x3 : 63,
-        y3 : 45
-    }
-    
-    triangleArr.push(triangle01);
 }
 
 function draw(){
     background(255);
-    image(img,0,0);
-    triangleArr.forEach((triangleEll)=>{
-        triangle(triangleEll.x1, triangleEll.y1, triangleEll.x2, triangleEll.y2, triangleEll.x3, triangleEll.y3);
-    })
+    image(beamBg,0,0);
+    image(rider01,rider01X,14);
+    //console.log(canvas.position());
+    // console.log(rider01);
+    // triangleArr.forEach((triangleEll)=>{
+    //     triangle(triangleEll.x1, triangleEll.y1, triangleEll.x2, triangleEll.y2, triangleEll.x3, triangleEll.y3);
+    // })
     
 }
 
 function mouseDragged(){
-    triangleArr = [];
-    triangle01 = {
-        x1 : mouseX - 9,
-        y1 : 27,
-        x2 : mouseX + 9,
-        y2 : 27,
-        x3 : mouseX,
-        y3 : 45
-    }
+    rider01X = mouseX - 40;
+    image(rider01, rider01X, 14);
+    // triangleArr = [];
+    // triangle01 = {
+    //     x1 : mouseX - 9,
+    //     y1 : 27,
+    //     x2 : mouseX + 9,
+    //     y2 : 27,
+    //     x3 : mouseX,
+    //     y3 : 45
+    // }
     
-    triangleArr.push(triangle01);
+    // triangleArr.push(triangle01);
+}
+
+function checkClick (rider){
+    // if(
+    //     mx <= rider.x
+    // ){}
+    
 }
